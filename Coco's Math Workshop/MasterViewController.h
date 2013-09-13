db874d70ab94f8d37001e972527d5407daf202c0
Kevin <kevinchh88@gmail.com>
2013-09-12 21:10:31 -0700

    Initial Commit

---
+++ Coco's Math Workshop/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Coco's Math Workshop
+//
+//  Created by Kevin Huang on 9/12/13.
+//  Copyright (c) 2013 The Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
